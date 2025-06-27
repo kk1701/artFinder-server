bcb4565a78875f6bf64b3823193aabc5f6ef6d1a
kk1701 <karankute05@gmail.com>
2025-06-27 13:25:57 +0530

    fix

--- YoutubeData/youtubeModel.py
+++ YoutubeData/youtubeModel.py
@@ -84,7 +84,7 @@ def youtube_model(data):
     X_train, X_test, y_train, y_test = train_test_split(X_scaled, label, test_size=0.2, random_state=42)
 
     model = LogisticRegression()
-    model.fit(X_train, y_train)
+    # model.fit(X_train, y_train)
 
     # y_pred = model.predict(X_test)
     # print(classification_report(y_test, y_pred))
